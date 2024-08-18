b341dde727c5066cdbcdf0dee6775828ce7d3acd
Ian Samir <falcnpawnch@gmail.com>
2024-08-18 13:45:20 -0400

    example post in posts

--- create_post.rb
+++ create_post.rb
@@ -1,9 +1,9 @@
 require 'date'
 
 def ensure_posts_directory_exists
-    Dir.mkdir('posts') unless Dir.exist?('posts')
-  end
-  
+  Dir.mkdir('posts') unless Dir.exist?('posts')
+end
+
 def find_latest_blog
   max_blog_num = 0
   Dir.glob('posts/blog*.html').each do |filename|
@@ -69,18 +69,19 @@ def update_index_html(new_blog_filename, title)
 end
 
 if __FILE__ == $0
-    ensure_posts_directory_exists
+  ensure_posts_directory_exists
 
-    if ARGV.empty?
-      puts "Please provide the title for the new blog post."
-      exit
-    end
-  
-    title = ARGV.join(" ")  # Joins all arguments into a single string, assuming the title may contain spaces
-    latest_blog_num = find_latest_blog
-    new_blog_num = latest_blog_num + 1
-    new_blog_filename = create_new_blog(new_blog_num, title)
-    update_index_html(new_blog_filename, title)
-    puts "Created '#{new_blog_filename}' and updated 'index.html' with title '#{title}'"
+  if ARGV.empty?
+    puts "Please provide the title for the new blog post."
+    exit
   end
+
+  title = ARGV.join(" ")  # Joins all arguments into a single string, assuming the title may contain spaces
+  latest_blog_num = find_latest_blog
+  new_blog_num = latest_blog_num + 1
+  new_blog_filename = create_new_blog(new_blog_num, title)
+  update_index_html(new_blog_filename, title)
+  puts "Created '#{new_blog_filename}' and updated 'index.html' with title '#{title}'"
+end
+
   
